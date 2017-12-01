f3d5696603a32407b47775359e4a29d42d6ec4e1
Luis <spartanhero93@gmail.com>
2017-12-01 15:32:47 -0500

    Improved user-readability

--- src/index.js
+++ src/index.js
@@ -12,40 +12,34 @@ const API_KEY         = 'YOUR_API_KEY';
 class App extends Component {
     constructor(props) {
         super(props);
-
         this.state = { 
             videos: [], 
             selectedVideo: null
         };
-
         this.videoSearch('')
     }
-
-    videoSearch(term){
-        YTSearch({ key: API_KEY, term: term}, (videos) => {
-            //Whith ES6 if the key and value (look below) they can be set to just that one term.
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
+videoSearch(term){
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+    //Whith ES6 if the key and value (look below) they can be set to just that one term.
+    this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
         });
-    }
-
-    render() {
-        const videoSearch = _.debounce((term) => { this.videoSearch(term) } , 300);
-
+    });
+}
+render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) } , 300);
     return (
         <div>
-            <SearchBar onSearchTermChange={videoSearch} />
-            <VideoDetail video={this.state.selectedVideo} />
-            <VideoList 
-                onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-                videos={this.state.videos} />
+         <SearchBar onSearchTermChange={videoSearch} />
+         <VideoDetail video={this.state.selectedVideo} />
+         <VideoList 
+         onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+         videos={this.state.videos} />
         </div>
-        );
+    );
     }
 }
-
 ReactDOM.render(<App />, document.querySelector('.container'))
 
 //State is a plain JS object to record and react to user events;
